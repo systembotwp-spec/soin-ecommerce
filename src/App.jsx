8f7de91061b4ffeff3960001783d2c45e974082d
systembotwp-spec <systembotwp@gmail.com>
2026-04-13 11:08:41 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { ShoppingCart } from "lucide-react";
+
+export default function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>SOIN Ecommerce 🚀</h1>
+      <button>
+        <ShoppingCart /> Carrito
+      </button>
+    </div>
+  );
+}
