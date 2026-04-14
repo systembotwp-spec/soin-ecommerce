5545dec5d0ebe6d061c6176c8e3ae7b2375f1368
systembotwp-spec <systembotwp@gmail.com>
2026-04-14 12:03:10 -0500

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <div style={{
+      background: "#2D4A35",
+      color: "#fff",
+      padding: 20,
+      textAlign: "center"
+    }}>
+      © SOIN - Todos los derechos reservados
+    </div>
+  );
+}
