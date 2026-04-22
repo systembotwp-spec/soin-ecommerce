dbbf7adbecd89343f0a7542b909193cbfae0ed37
systembotwp-spec <systembotwp@gmail.com>
2026-04-21 20:52:57 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,10 @@
-Vite(import { defineConfig } from 'vite' import react from '@vitejs/plugin-react' export default defineConfig({ plugins: [react()], build: { emptyOutDir: true } }))
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Aseguramos explícitamente la carpeta de salida
+    emptyOutDir: true,
+  }
+})
